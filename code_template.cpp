d9042f0cd89f9f33eb601019d62e5abdaa34873c
mohitsvnit <mohitsvnit093@gmail.com>
2019-08-23 18:40:55 +0530

    added code template

---
+++ code_template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <string>
+using namespace std;
+#define FAST_INPUT_OUTPUT ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+// Write your code below
+
+
+void solve() {
+
+}
+
+
+// END Code
+int main() {
+    FAST_INPUT_OUTPUT;
+    solve();
+    return 0;
+}
